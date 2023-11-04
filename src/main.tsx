028f19cd24b8e4fce3c2c144b4398a55444521eb
Felix <fb52683@fer.hr>
2023-11-04 01:00:41 +0100

    Update configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
-
 import { ThemeProvider } from "@tiller-ds/theme";
-import "./index.css";
 
+import App from "./App";
+import "./index.css";
 import {
   defaultComponentConfig,
   defaultIconConfig,
