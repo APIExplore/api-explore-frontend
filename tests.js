dc8d41b64748199266d9bdec4e71eff1fab17d8a
Marko <marko.uremovic00@gmail.com>
2023-12-18 20:19:35 +0100

    testing bottom panel

--- tests.js
+++ tests.js
@@ -7,6 +7,7 @@ try {
 
   const failed = await runner
     .src([
+      "./tests/bottom-panel-test.ts",
       "./tests/left-panel-test.ts",
       "./tests/middle-panel-test.ts",
       "./tests/right-panel-test.ts",
@@ -16,7 +17,7 @@ try {
     ])
     .browsers(["chrome --start-fullscreen"])
     .useProxy("localhost:5173")
-    .run({ speed: 0.9 });
+    .run({ speed: 0.7 });
 
   console.log("Tests failed: " + failed);
 } finally {
