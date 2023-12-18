63df015154dfcec97546db9b6f54ceead0c8c108
Marko <marko.uremovic00@gmail.com>
2023-12-18 19:38:55 +0100

    middle panel testing

--- tests.js
+++ tests.js
@@ -7,6 +7,7 @@ try {
 
   const failed = await runner
     .src([
+      "./tests/middle-panel-test.ts",
       "./tests/right-panel-test.ts",
       "./tests/test-new-old-schema.ts",
       "./tests/agent-test.ts",
