f215d13b821e87dbed8ebeb0d65ba4679919d0fd
Felix <fb52683@fer.hr>
2023-12-24 00:22:58 +0100

    Fix yarn build

--- src/components/LandingPage/newSchema.tsx
+++ src/components/LandingPage/newSchema.tsx
@@ -101,7 +101,6 @@ export default function NewSchema({
           onChange={onApiSchemaNameChange}
           placeholder="API schema name"
           value={name}
-          crossOrigin={undefined}
         />
       </div>
       <hr />
@@ -117,7 +116,6 @@ export default function NewSchema({
           onChange={onApiSchemaInputChange}
           placeholder="API schema adress"
           value={apiSchema}
-          crossOrigin={undefined}
         />
         <StatusButton
           status={isFetched ? "success" : inputError ? "error" : "idle"}
